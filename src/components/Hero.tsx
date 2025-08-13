8d95441a9334edc310db6086948845c8064e4a76
ZaiDi <zaidrafiq11@gmail.com>
2025-08-13 14:58:13 +0500

    new fixes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -86,7 +86,7 @@ const Hero = () => {
                 that users love.
               </motion.p>
 
-              {/* CTA Buttons */}
+              {/* CTA Buttons ---- */}
               <motion.div
                 initial={{ opacity: 0, y: 20 }}
                 animate={{ opacity: 1, y: 0 }}
